0be1158949d4ec82972148433b93f77590e8ad83
SathishSaminathan <Sathishcse1975@gmail.com>
2019-05-13 15:47:14 +0530

    initial_commit

---
+++ src/components/PreLoader/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import './PreLoader.css'
+
+class PreLoader extends Component {
+    render () {
+        return (
+            <div className="container">
+                
+            </div>
+        )
+    }
+}
+
+export default PreLoader
